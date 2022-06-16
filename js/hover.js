2363ca3c77b0146283bcf6ef92b1bedab958c95c
em-stea <emiliastea@hotmail.com>
2022-06-16 18:07:34 -0300

    upd

---
+++ js/hover.js
@@ -0,0 +1,59 @@
+$(".boton-1").hover(function () {
+  $(".tarjeta-1").css({
+    height: "654px",
+  });
+  $(".tarjeta-2").css({
+    height: "439px",
+  });
+  $(".tarjeta-3").css({
+    height: "439px",
+  });
+});
+
+$(".boton-2").hover(function () {
+  $(".tarjeta-2").css({
+    height: "654px",
+  });
+  $(".tarjeta-1").css({
+    height: "439px",
+  });
+  $(".tarjeta-3").css({
+    height: "439px",
+  });
+});
+
+$(".boton-3").hover(function () {
+  $(".tarjeta-3").css({
+    height: "654px",
+  });
+  $(".tarjeta-2").css({
+    height: "439px",
+  });
+  $(".tarjeta-1").css({
+    height: "439px",
+  });
+});
+
+$(".boton-4").hover(function () {
+  $(".tarjeta-3").css({
+    height: "439px",
+  });
+  $(".tarjeta-1").css({
+    height: "439px",
+  });
+  $(".tarjeta-2").css({
+    height: "439px",
+  });
+});
+
+$(".boton-5").hover(function () {
+  $(".tarjeta-3").css({
+    height: "439px",
+  });
+  $(".tarjeta-2").css({
+    height: "439px",
+  });
+  $(".tarjeta-1").css({
+    height: "439px",
+  });
+});
